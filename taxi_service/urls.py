dc62513d335ba0f30bce008a61a148235cfd24b8
Sasha <sasha.ryabokon14@gmail.com>
2026-08-10 15:21:49 +0300

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,11 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts:login/",
-         LoginView.as_view(template_name="registration/login.html"),
-         name="login"),
-    path("accounts:logout/",
-         LogoutView.as_view(next_page="taxi:index"), name="logout"),
     path("", include(("taxi.urls"), namespace="taxi")),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
